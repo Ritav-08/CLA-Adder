280e81e9c5765472ffe7f387715cceb8475992a5
Ritav-08 <ritav08092004@gmail.com>
2026-04-13 16:09:37 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.04.2026 13:08:27
-// Design Name: 
-// Module Name: Design
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module claAdder(
     input [3:0]a_i, 
