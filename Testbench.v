8199795a89a3fdc8f5ed31d40a8918d469bfb2ec
Ritav-08 <ritav08092004@gmail.com>
2026-04-13 16:09:53 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.04.2026 13:09:16
-// Design Name: 
-// Module Name: tb_claAdder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_claAdder();
 reg [3:0]a_i;
